0c8fce87576602ab51ebe356ce20d4b86d13656e
Davis SHYAKA <shyakakingdavis@gmail.com>
2022-04-12 09:59:30 +0200

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "alx.h"
+
+/**
+ * print_alpahbet_x10 - print the alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int round = 0;
+	char letter = 'a';
+
+	while (round < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		round++;
+	}
+}
