44737cec1a8990afbbe0cfd840cf819d91380ad8
Davis SHYAKA <shyakakingdavis@gmail.com>
2022-04-13 13:41:22 +0200

    Task 0 - Fix.02

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
-/*
- * main - prints a random number
- *
- * Description: prints and test whether it's postive, negative or 0
- *
- * Return: Always 0 (Success)
- */
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int n;
+        int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	positive_or_negative(0);
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
