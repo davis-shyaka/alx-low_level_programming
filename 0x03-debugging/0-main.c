c350f28e28234d45b7e0a2eea98c1bda2aa6ec72
Davis SHYAKA <shyakakingdavis@gmail.com>
2022-04-13 13:19:23 +0200

    Initial commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <main.h>
+
+/*
+ * main - prints a random number
+ *
+ * Description: prints and test whether it's postive, negative or 0
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	positive_or_negative(0);
+	return (0);
+}
