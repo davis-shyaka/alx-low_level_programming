610905da268b5f524955c05add5d31f24e61d4b3
Davis SHYAKA <shyakakingdavis>
2022-04-27 10:32:30 +0200

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,22 +10,22 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	char *ahaystack;
-	char *bneedle;
+	char *bhaystack;
+	char *pneedle;
 
 	while (*haystack != '\0')
 	{
-		ahaystack = haystack;
-		bneedle = needle;
+		bhaystack = haystack;
+		pneedle = needle;
 
-		while (*haystack != '\0' && *bneedle != '\0' && *haystack == *bneedle)
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
 		{
 			haystack++;
 			pneedle++;
 		}
-		if (!*bneedle)
-			return (ahaystack);
-		haystack = ahaystack + 1;
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
 	}
 	return (0);
 }
