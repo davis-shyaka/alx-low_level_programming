a29fed5710bb7f0705cf034fd7859360af746ea2
Davis SHYAKA <shyakakingdavis>
2022-05-16 19:04:02 +0200

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,11 +11,11 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
-	va_list valist;
+	va_list vl;
 
-	va_start(valist, n);
+	va_start(vl, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
-	va_end(valist);
+		sum += va_arg(vl, int);
+	va_end(vl);
 	return (sum);
 }
